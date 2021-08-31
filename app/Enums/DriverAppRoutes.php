3205deba87d5fe195617cdfe858676e02f22fea6
Hamlet Minjares <hamlet@deadline.com.mx>
2021-08-31 05:16:47 -0600

    Minor adjustments for app smart linking redirect.

--- app/Enums/DriverAppRoutes.php
+++ app/Enums/DriverAppRoutes.php
@@ -8,6 +8,7 @@ abstract class DriverAppRoutes
 
     public const NOTIFICATIONS = "/notifications";
     public const LOAD = "/load";
+    public const LOAD_ID = "load";
     public const CHAT = "/dashboard?index=1";
     public const CHAT_ID = 'chat';
 
