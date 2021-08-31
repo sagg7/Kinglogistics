4303eede75344f8f3cd762d68a2bf8819079a000
joseKipup <angel.lujan@kipup.mx>
2021-08-31 12:14:48 -0600

    Add roles to access tracking and chat

--- routes/channels.php
+++ routes/channels.php
@@ -19,8 +19,8 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('driver-location-king', function ($user) {
-    // TODO: Check if user is admin
-    return (bool)$user->hasRole('admin');
+    // TODO: Check if user is admin, operations or dispatch
+    return (bool)$user->hasRole(['admin', 'operations', 'dispatch']);
 });
 
 Broadcast::channel('driver-location-carrier.{locationGroupId}', function ($carrier, $carrier_id) {
@@ -35,7 +35,7 @@ Broadcast::channel('driver-location-shipper.{locationGroupId}', function ($shipp
 });
 
 Broadcast::channel('chat', function ($user) {
-    // TODO: Check if user is admin
-    return (bool)$user->hasRole('admin');
+    // TODO: Check if user is admin, operations or dispatch
+    return (bool)$user->hasRole(['admin', 'operations', 'dispatch']);
 });
 
--- routes/web/tracking.php
+++ routes/web/tracking.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\TrackingController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('tracking')->group(function () {
-    Route::get('/', [TrackingController::class, 'index'])
-        ->name('tracking.index');
+Route::group(['middleware' => ['role:admin|operations|dispatch']], function () {
+    Route::prefix('tracking')->group(function () {
+        Route::get('/', [TrackingController::class, 'index'])
+            ->name('tracking.index');
+    });
 });
