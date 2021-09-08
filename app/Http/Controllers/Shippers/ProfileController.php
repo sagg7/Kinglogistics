5dbe149912769c673efce92c54f0eff784b79a34
joseKipup <angel.lujan@kipup.mx>
2021-09-08 10:33:41 -0600

    Add image rendering correctly for equipment and services

--- app/Http/Controllers/Shippers/ProfileController.php
+++ app/Http/Controllers/Shippers/ProfileController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers\Shippers;
 use App\Http\Controllers\Controller;
 use App\Models\Equipment;
 use App\Models\Service;
+use App\Traits\Storage\S3Functions;
+use DOMDocument;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    use S3Functions;
+
     protected $broker_id;
 
     public function __construct()
@@ -28,9 +32,25 @@ class ProfileController extends Controller
         $equipment = Equipment::where('broker_id', $this->broker_id)
             ->first([
                 'title',
-                'message as html'
+                'message'
             ]);
-        !$equipment ? $equipment = [] : $equipment = $equipment->toArray();
+        if (!$equipment) {
+            $equipment = [];
+        } else {
+            $dom = new DOMDocument();
+            $dom->loadHTML($equipment->message);
+
+            foreach ($dom->getElementsByTagName('img') as $img) {
+                $img->setAttribute('src', $this->getTemporaryFile(substr($img->getAttribute('src'),1)));
+                $img->setAttribute('class', 'img-fluid');
+            }
+
+            foreach ($dom->getElementsByTagName('blockquote') as $item) {
+                $item->setAttribute('class', 'blockquote pl-1 border-left-primary border-left-3');
+            }
+            $equipment->html = $dom->saveHTML();
+            $equipment = $equipment->toArray();
+        }
         $params = ['section' => 'Equipment'] + $equipment;
         return view('layouts.renderHtml', $params);
     }
@@ -40,9 +60,25 @@ class ProfileController extends Controller
         $service = Service::where('broker_id', $this->broker_id)
             ->first([
                 'title',
-                'message as html'
+                'message',
             ]);
-        !$service ? $service = [] : $service = $service->toArray();
+        if (!$service) {
+            $service = [];
+        } else {
+            $dom = new DOMDocument();
+            $dom->loadHTML($service->message);
+
+            foreach ($dom->getElementsByTagName('img') as $img) {
+                $img->setAttribute('src', $this->getTemporaryFile(substr($img->getAttribute('src'),1)));
+                $img->setAttribute('class', 'img-fluid');
+            }
+
+            foreach ($dom->getElementsByTagName('blockquote') as $item) {
+                $item->setAttribute('class', 'blockquote pl-1 border-left-primary border-left-3');
+            }
+            $service->html = $dom->saveHTML();
+            $service = $service->toArray();
+        }
         $params = ['section' => 'Services'] + $service;
         return view('layouts.renderHtml', $params);
     }
