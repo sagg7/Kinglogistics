69528bf02236e67a54a172de8cd70e9e5351e709
Hamlet Minjares <hamlet@deadline.com.mx>
2021-09-14 11:49:55 -0600

    Missed properties in create shift method. Fix for chat to retrieve the chat history.

--- app/Http/Resources/Drivers/ProfileResource.php
+++ app/Http/Resources/Drivers/ProfileResource.php
@@ -22,6 +22,8 @@ class ProfileResource extends JsonResource
             'inactive' => $this->phone,
             'carrier' => new CarrierResource($this->carrier),
             'zone' => new ZoneResource($this->zone),
+            'is_shift_active' => $this->isShiftActive(),
+            'has_active_loads' => $this->hasActiveLoads(),
             'created_at' => $this->created_at->format('m/d/Y'),
         ];
     }
