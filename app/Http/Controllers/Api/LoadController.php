e527f2047cf1c86dfe7b48f7566519d21927f202
Saul Garcia <garciasaulalejandro@kipup.mx>
2021-09-15 15:13:11 -0600

    add Ton

--- app/Http/Controllers/Api/LoadController.php
+++ app/Http/Controllers/Api/LoadController.php
@@ -271,6 +271,7 @@ class LoadController extends Controller
         $load = Load::find($loadId);
         $load->customer_reference = $request->get('sand_ticket');
         $load->weight = $request->get('weight');
+        $load->tons = floatval($request->get('weight'))/2000;
         $load->update();
 
         $loadStatus = $this->switchLoadStatus($loadId, LoadStatusEnum::ARRIVED);
