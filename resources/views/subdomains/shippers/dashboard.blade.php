fa09dfa4ccbe33556b3a44cb81d69759365ce4e1
joseKipup <angel.lujan@kipup.mx>
2021-09-15 13:48:59 -0600

    Fix for clicking dashboard load status summary

--- resources/views/subdomains/shippers/dashboard.blade.php
+++ resources/views/subdomains/shippers/dashboard.blade.php
@@ -60,7 +60,7 @@
                 chart.render();
             })();
         </script>
-        <script src="{{ asset('js/sections/dashboard/common.min.js?1.0.1') }}"></script>
+        <script src="{{ asset('js/sections/dashboard/common.min.js?1.0.2') }}"></script>
     @endsection
 
     @section('modals')
