6705bc8130438b744e71aa5afc79eba8dfc8ab50
Saul Garcia <garciasaulalejandro@kipup.mx>
2021-11-22 09:29:22 -0700

    test 9:31

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -106,7 +106,7 @@ class Kernel extends ConsoleKernel
             }
 
             //BotLoadReminder::dispatch([$driver->id])->delay(now()->addMinutes(AppConfig::where('key', AppConfigEnum::TIME_AFTER_LOAD_REMINDER)->first()/60));
-        })->daily()->at('09:20');
+        })->daily()->at('9:31');
 
 
         $schedule->call(function () {
