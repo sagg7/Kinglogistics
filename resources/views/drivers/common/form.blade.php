6b8bf0d8a2a1eb3651ede3e30ad75658aec35ae1
Saul Garcia <garciasaulalejandro@kipup.mx>
2021-11-30 16:21:48 -0700

    fix drivers error

--- resources/views/drivers/common/form.blade.php
+++ resources/views/drivers/common/form.blade.php
@@ -14,7 +14,7 @@
                     </div>
                     <div class="form-group col-md-6">
                         {!! Form::label('turn_id', ucfirst(__('turn')), ['class' => 'col-form-label']) !!}
-                        {!! Form::select('turn_id', $turns, $driver->turn_id ?? null, ['class' => 'form-control select2' . ($errors->first('turn_id') ? ' is-invalid' : '')]) !!}
+                        {!! Form::select('turn_id', $turns, isset($driver) ? $driver->turn_id : null, ['class' => 'form-control select2' . ($errors->first('turn_id') ? ' is-invalid' : '')]) !!}
                         @error('turn_id')
                         <span class="invalid-feedback" role="alert">
                         <strong>{{ ucfirst($message) }}</strong>
@@ -23,7 +23,7 @@
                     </div>
                     <div class="form-group col-md-6">
                         {!! Form::label('zone_id', ucfirst(__('zone')), ['class' => 'col-form-label']) !!}
-                        {!! Form::select('zone_id', $zones, $driver->zone_id ?? null, ['class' => 'form-control' . ($errors->first('zone_id') ? ' is-invalid' : '')]) !!}
+                        {!! Form::select('zone_id', $zones, isset($driver) ? $driver->zone_id : null, ['class' => 'form-control' . ($errors->first('zone_id') ? ' is-invalid' : '')]) !!}
                         @error('zone_id')
                         <span class="invalid-feedback" role="alert">
                         <strong>{{ ucfirst($message) }}</strong>
@@ -33,7 +33,7 @@
                 @endif
                 <div class="form-group col-md-6">
                     {!! Form::label('name', ucfirst(__('name')), ['class' => 'col-form-label']) !!}
-                    {!! Form::text('name', $driver->name ?? null, ['class' => 'form-control' . ($errors->first('name') ? ' is-invalid' : '')]) !!}
+                    {!! Form::text('name', isset($driver) ? $driver->name : null, ['class' => 'form-control' . ($errors->first('name') ? ' is-invalid' : '')]) !!}
                     @error('name')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ ucfirst($message) }}</strong>
@@ -42,7 +42,7 @@
                 </div>
                 <div class="form-group col-md-6">
                     {!! Form::label('email', ucfirst(__('email')), ['class' => 'col-form-label']) !!}
-                    {!! Form::email('email', $driver->email ?? null, ['class' => 'form-control' . ($errors->first('email') ? ' is-invalid' : '')]) !!}
+                    {!! Form::email('email', isset($driver) ? $driver->email : null, ['class' => 'form-control' . ($errors->first('email') ? ' is-invalid' : '')]) !!}
                     @error('email')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ ucfirst($message) }}</strong>
@@ -71,19 +71,28 @@
             </div>
             <hr>
             <div class="row">
-                <div class="form-group col-md-6">
+                <div class="form-group col-md-4">
                     {!! Form::label('phone', ucfirst(__('phone')), ['class' => 'col-form-label']) !!}
-                    {!! Form::text('phone', $driver->phone ?? null, ['class' => 'form-control' . ($errors->first('phone') ? ' is-invalid' : '')]) !!}
+                    {!! Form::text('phone', isset($driver) ? $driver->phone : null, ['class' => 'form-control' . ($errors->first('phone') ? ' is-invalid' : '')]) !!}
                     @error('phone')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ ucfirst($message) }}</strong>
                     </span>
                     @enderror
                 </div>
-                <div class="form-group col-md-6">
+                <div class="form-group col-md-4">
                     {!! Form::label('address', ucfirst(__('address')), ['class' => 'col-form-label']) !!}
-                    {!! Form::text('address', $driver->address ?? null, ['class' => 'form-control' . ($errors->first('address') ? ' is-invalid' : '')]) !!}
+                    {!! Form::text('address', isset($driver) ? $driver->address : null, ['class' => 'form-control' . ($errors->first('address') ? ' is-invalid' : '')]) !!}
                     @error('address')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ ucfirst($message) }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="form-group col-md-4">
+                    {!! Form::label('language', ucfirst(__('language')), ['class' => 'col-form-label']) !!}
+                    {!! Form::select('language', $language, isset($driver) ? $driver->language : null, ['class' => 'form-control select2' . ($errors->first('turn_id') ? ' is-invalid' : '')]) !!}
+                    @error('language')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ ucfirst($message) }}</strong>
                     </span>
@@ -91,9 +100,9 @@
                 </div>
                 <div class="form-group col-md-6">
                     <fieldset>
-                        {!! Form::label('inactive', ucfirst(__('inactive')), ['class' => 'col-form-label']) !!}
+                        {!! Form::label('inactive', ucfirst(__('Inactive Down')), ['class' => 'col-form-label']) !!}
                         <div class="vs-checkbox-con vs-checkbox-primary">
-                            {{ Form::checkbox('inactive', 1, $driver->inactive ?? null) }}
+                            {{ Form::checkbox('inactive', 1, isset($driver) ? $driver->inactive : null) }}
                             <span class="vs-checkbox">
                                 <span class="vs-checkbox--check">
                                     <i class="vs-icon feather icon-check"></i>
@@ -104,7 +113,7 @@
                 </div>
                 <div class="form-group col-12">
                     {!! Form::label('inactive_observations', ucfirst(__('inactive observations')), ['class' => 'col-form-label']) !!}
-                    {!! Form::textarea('inactive_observations', $driver->inactive_observations, ['class' => 'form-control', 'rows' => 5, 'maxlength' => 512]) !!}
+                    {!! Form::textarea('inactive_observations', isset($driver) ? $driver->inactive_observations : null, ['class' => 'form-control', 'rows' => 5, 'maxlength' => 512]) !!}
                     @error('inactive_observations')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ ucfirst($message) }}</strong>
