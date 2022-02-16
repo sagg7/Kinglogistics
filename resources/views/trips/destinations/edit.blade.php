9000fa1709d60452390109529b37c5a0c20b8bce
joseKipup <angel.lujan@kipup.mx>
2022-02-16 16:44:28 -0700

    Fix for chat view for duplicated const variable

---
+++ resources/views/trips/destinations/edit.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="crumb_section">Origin</x-slot>
+    <x-slot name="crumb_subsection">Create</x-slot>
+
+    @section('scripts')
+        <script src="https://maps.googleapis.com/maps/api/js?key={{ env("GOOGLE_MAPS_API") }}&libraries=places"></script>
+        <script src="{{ asset('js/sections/trips/common.min.js?1.0.1') }}"></script>
+        <script src="{{ asset('js/common/typesModal.min.js') }}"></script>
+        <script src="{{ asset('js/sections/trips/location.min.js') }}"></script>
+    @endsection
+
+    {!! Form::open(['route' => ['origin.update', $origin->id], 'method' => 'post', 'class' => 'form form-vertical', 'id' => 'coordsForm']) !!}
+    @include('trips.common.originDestinationForm', ['model' => $origin])
+    {!! Form::close() !!}
+</x-app-layout>
