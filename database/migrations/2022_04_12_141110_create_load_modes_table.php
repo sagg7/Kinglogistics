afce08f32b2a4807cb3d39b4d6c504241e3d4b2c
joseKipup <angel.lujan@kipup.mx>
2022-04-14 12:40:55 -0600

    Correction for migration naming on load_modes migration

--- database/migrations/2022_04_12_141110_create_load_modes_table.php
+++ database/migrations/2022_04_12_141110_create_load_modes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoadLoadsModeTable extends Migration
+class CreateLoadModesTable extends Migration
 {
     /**
      * Run the migrations.
