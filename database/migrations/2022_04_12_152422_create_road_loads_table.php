fc1e305dc51f575b1078fce6efb05a790e801e37
joseKipup <angel.lujan@kipup.mx>
2022-04-14 12:38:23 -0600

    Update road loads migration

--- database/migrations/2022_04_12_152422_create_road_loads_table.php
+++ database/migrations/2022_04_12_152422_create_road_loads_table.php
@@ -16,14 +16,28 @@ class CreateRoadLoadsTable extends Migration
         Schema::create('road_loads', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('load_id');
+            $table->unsignedBigInteger('trailer_type_id');
+            $table->unsignedBigInteger('mode_id')->nullable();
+            $table->unsignedBigInteger('origin_city_id')->nullable();
+            $table->unsignedBigInteger('destination_city_id')->nullable();
             $table->unsignedInteger('deadhead_miles')->nullable();
-            $table->unsignedInteger('trailer_type');
             $table->enum('load_size', ['full', 'partial']);
             $table->unsignedInteger('length')->nullable();
             $table->unsignedDecimal('pay_rate')->nullable();
+            $table->timestamp('origin_late_pick_up_date');
+            $table->timestamp('destination_late_pick_up_date');
+            $table->timestamp('origin_early_pick_up_date');
+            $table->timestamp('destination_early_pick_up_date');
+            $table->unsignedInteger('width');
+            $table->unsignedInteger('height');
+            $table->unsignedInteger('cube');
+            $table->unsignedInteger('pieces');
+            $table->unsignedInteger('pallets');
             $table->timestamps();
 
             $table->foreign('load_id')->references('id')->on('loads')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('trailer_type_id')->references('id')->on('load_trailer_types')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('mode_id')->references('id')->on('load_modes')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
